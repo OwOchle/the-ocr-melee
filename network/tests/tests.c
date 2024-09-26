1465038c0341fdcece42e670372bdc57feea6aff
OwOchlé <romeoboub@gmail.com>
2024-09-26 22:24:22 +0200

    test: added tests directory and tests target for unit and handtesting

---
+++ network/tests/tests.c
@@ -0,0 +1,28 @@
+#include "array.h"
+#include "matrix.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void test1()
+{
+    float mat1[] = {1, 2, 3, 4, 5, 6};
+    size_t width1 = 3;
+    size_t height1 = 2;
+
+    float mat2[] = {7, 8, 9, 10, 11, 12};
+    size_t width2 = 2;
+    size_t height2 = 3;
+
+    float *res = matrix_multiply(width1, height1, mat1, width2, height2, mat2);
+
+    array_float_print(height1 * width2, res);
+
+    free(res);
+}
+
+int main()
+{
+    test1();
+    return EXIT_SUCCESS;
+}
