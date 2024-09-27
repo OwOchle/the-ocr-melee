6d943b9b830583e5be496196ca315857da87502e
Bastien1533 <bastien.pinaud@yahoo.com>
2024-09-28 00:31:03 +0200

    feat(wip): :construction:  Basic file reading in stdout.

--- solver/main.c
+++ solver/main.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <err.h>
+
+int BUFFER_SIZE = 1;
+
+void read_file(char **filename){
+    int fd = open(filename, O_RDONLY);
+
+    if (fd == -1){
+        err(1, "open() : File Not Found");
+    }
+    int r, w, n;
+    char buffer[BUFFER_SIZE];
+    while ((r = read(fd, buffer, BUFFER_SIZE)) != 0)
+    {
+        w = write(STDOUT_FILENO, buffer, r);
+        n++;
+        if (w == -1){
+            err(1, "write() : Error in file read.");
+        }
+    }
+    close(fd);
+}
 
 int main(int argc, char **argv)
 {
-    printf("Hello world!\n");
+    if (argc < 2){
+        err(1, "Solver usage : ./solver <filename>");
+    }
+    read_file(argv[1]);
     return 0;
 }
