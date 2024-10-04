f5802eadf13feb5c708612c6109afef25d20bac0
Christina Michelard <christina.michelard@epita.fr>
2024-10-04 15:15:19 +0200

    initial commit

---
+++ image_processing/main.c
@@ -0,0 +1,11 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(int argc, char *argv[])
+{
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+    SDL_Texture *texutre = NULL;
+     
+}
