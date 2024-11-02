43eac7b5446762ea8b0593ce5ebff29f96f6578d
Christina-Nora Michelard <christina.michelard@epita.fr>
2024-11-02 19:00:06 +0100

    feat(solver): last fixes done?

--- solver/solver.c
+++ solver/solver.c
@@ -14,52 +14,56 @@ int find_word_matrix(char* matrix, int rows, int columns, char* word, int word_s
             int up = 0; // 0 => can't proceed downwards for the diags
             int down = 0; // 0 => can't proceed upwards for the diags
 
-            // Check upwards
-            if (x - word_size + 1 >= 0)
+            if (matrix[x*columns + y] == word[0]) // Check if the first letter corresponds
             {
-                if (find_word_matrix_u(matrix, rows, columns, word, word_size, x, y, arr) == 1){
-                    return 1;
-                }
-                up = 1; // The solver can proceed upwards for the 2 diags
-            }    
+                // Check upwards
+                if (x - word_size + 1 >= 0)
+                {
+                    if (find_word_matrix_u(matrix, rows, columns, word, word_size, x, y, arr) == 1){
+                        return 1;
+                    }
+                    up = 1; // The solver can proceed upwards for the 2 diags
+                }    
 
-            // Check downwards
-            if (x + word_size - 1 < rows)
-            {
-                if (find_word_matrix_d(matrix, rows, columns, word, word_size, x, y, arr) == 1){
-                    return 1;
+                // Check downwards
+                if (x + word_size - 1 < rows)
+                {
+                    if (find_word_matrix_d(matrix, rows, columns, word, word_size, x, y, arr) == 1){
+                        return 1;
+                    }
+                    down = 1; // The solver can proceed downwards for the 2 diags
                 }
-                down = 1; // The solver can proceed downwards for the 2 diags
+
+                // Check leftwards
+                if (y - word_size + 1 >= 0)
+                {
+                    if (find_word_matrix_l(matrix, rows, columns, word, word_size, x, y, arr) == 1){
+                        return 1;
+                    }
+                    if (up == 1 && find_word_matrix_lu(matrix, rows, columns, word, word_size, x, y, arr) == 1){ // left-upward diagonal
+                        return 1;
+                    }
+                    if (down == 1 && find_word_matrix_ld(matrix, rows, columns, word, word_size, x, y, arr) == 1){ // left-downward diagonal
+                        return 1;
+                    }
+
+                }
+
+                // Check rightwards
+                if (y + word_size - 1 < columns)
+                {
+                    if (find_word_matrix_r(matrix, rows, columns, word, word_size, x, y, arr) == 1){
+                        return 1;
+                    }
+                    if (up == 1 && find_word_matrix_ru(matrix, rows, columns, word, word_size, x, y, arr) == 1){ // right-upward diagonal
+                        return 1;
+                    }
+                    if (down == 1 && find_word_matrix_rd(matrix, rows, columns, word, word_size, x, y, arr) == 1){ // right-downward diagonal
+                        return 1;
+                    }
+                }
             }
-
-            // Check leftwards
-            if (y - word_size + 1 >= 0)
-            {
-                if (find_word_matrix_l(matrix, rows, columns, word, word_size, x, y, arr) == 1){
-                    return 1;
-                }
-                if (up == 1 && find_word_matrix_lu(matrix, rows, columns, word, word_size, x, y, arr) == 1){ // left-upward diagonal
-                    return 1;
-                }
-                if (down == 1 && find_word_matrix_ld(matrix, rows, columns, word, word_size, x, y, arr) == 1){ // left-downward diagonal
-                    return 1;
-                }
-
-            }
-
-            // Check rightwards
-            if (y + word_size - 1 < columns)
-            {
-                if (find_word_matrix_r(matrix, rows, columns, word, word_size, x, y, arr) == 1){
-                    return 1;
-                }
-                if (up == 1 && find_word_matrix_ru(matrix, rows, columns, word, word_size, x, y, arr) == 1){ // right-upward diagonal
-                    return 1;
-                }
-                if (down == 1 && find_word_matrix_rd(matrix, rows, columns, word, word_size, x, y, arr) == 1){ // right-downward diagonal
-                    return 1;
-                }
-            }
+            
 
         }
     }
@@ -97,7 +101,7 @@ int find_word_matrix_r(char* matrix, int rows, int columns, char* word, int word
     }
     if (word_index == word_size){
         arr->xend = x;
-        arr->yend = i;
+        arr->yend = i - 1;
         return 1;
     }
     else
@@ -134,7 +138,7 @@ int find_word_matrix_d(char* matrix, int rows, int columns, char* word, int word
         verbose_printf("word index = %i\n", word_index);
     }
     if (word_index == word_size){
-        arr->xend = i;
+        arr->xend = i - 1;
         arr->yend = y;
         return 1;
     }
@@ -155,7 +159,7 @@ int find_word_matrix_ru(char* matrix, int rows, int columns, char* word, int wor
     }
     if (word_index == word_size){
         arr->xend = i + 1;
-        arr->yend = j;
+        arr->yend = j - 1;
         return 1;
     }
     else
@@ -194,8 +198,8 @@ int find_word_matrix_rd(char* matrix, int rows, int columns, char* word, int wor
         word_index++;
     }
     if (word_index == word_size){
-        arr->xend = i;
-        arr->yend = j;
+        arr->xend = i - 1;
+        arr->yend = j - 1;
         return 1;
     }
     else
@@ -214,7 +218,7 @@ int find_word_matrix_ld(char* matrix, int rows, int columns, char* word, int wor
         word_index++;
     }
     if (word_index == word_size){
-        arr->xend = i;
+        arr->xend = i - 1;
         arr->yend = j + 1;
         return 1;
     }
