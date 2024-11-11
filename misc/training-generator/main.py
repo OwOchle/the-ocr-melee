5cfe6340866a8213e31b96fd32e97ecc91e47f5c
OwOchlé <romeoboub@gmail.com>
2024-11-11 01:46:25 +0100

    fix: windows fix

--- misc/training-generator/main.py
+++ misc/training-generator/main.py
@@ -3,34 +3,30 @@ import random
 import os
 
 fonts = {
-    "roboto": "./fonts/Roboto-Regular.ttf",
-    "notoserif": "./fonts/NotoSerif-Regular.ttf",
-    "sourgummy": "./fonts/SourGummy-Regular.ttf",
-    "jbm": "./fonts/JetBrainsMono-Regular.ttf"
+    "roboto": ImageFont.truetype("./fonts/Roboto-Regular.ttf", 20),
+    "notoserif": ImageFont.truetype("./fonts/NotoSerif-Regular.ttf", 20),
+    "sourgummy": ImageFont.truetype("./fonts/SourGummy-Regular.ttf", 20),
+    "jbm": ImageFont.truetype("./fonts/JetBrainsMono-Regular.ttf", 20)
     }
 
 OUTPUT = "./outputs"
 
 chars = [chr(c) for c in range(ord("A"), ord("Z")+1)] # + [chr(c) for c in range(ord("a"), ord("z")+1)]
 
-def generate_plain(f_file, char) -> Image.Image:
+def generate_plain(font, char) -> Image.Image:
     img = Image.new("RGB", (25, 25), (255, 255, 255))
 
     d = ImageDraw.Draw(img)
-
-    font = ImageFont.truetype(f_file, 20)
 
     d.text((12, 12), char, anchor="mm", fill=(0,0,0), font=font, align="center")
 
     return img
 
 
-def generate_offcenter(f_file, char) -> Image.Image:
+def generate_offcenter(font, char) -> Image.Image:
     img = Image.new("RGB", (25, 25), (255, 255, 255))
 
     d = ImageDraw.Draw(img)
-
-    font = ImageFont.truetype(f_file, 20)
 
     x = random.randint(7, 17)
     y = random.randint(7, 17)
@@ -40,12 +36,10 @@ def generate_offcenter(f_file, char) -> Image.Image:
     return img
 
 
-def generate_rotated(f_file, char) -> Image.Image:
+def generate_rotated(font, char) -> Image.Image:
     img = Image.new("RGB", (25, 25), (255, 255, 255))
 
     d = ImageDraw.Draw(img)
-
-    font = ImageFont.truetype(f_file, 20)
 
     d.text((12, 12), char, anchor="mm", fill=(0,0,0), font=font, align="center")
 
@@ -58,12 +52,12 @@ def main():
     if not os.path.exists(OUTPUT):
         os.mkdir(OUTPUT)
 
-    for f_name, f_file in fonts.items():
+    for f_name, f in fonts.items():
         for char in chars:
-            generate_plain(f_file, char).save(f"{OUTPUT}/{f_name}_plain_0_{char}.bmp")
+            generate_plain(f, char).save(f"{OUTPUT}/{f_name}_plain_0_{char}.bmp")
             for i in range(3):
-                generate_offcenter(f_file, char).save(f"{OUTPUT}/{f_name}_offcenter_{i}_{char}.bmp")
-                generate_rotated(f_file, char).save(f"{OUTPUT}/{f_name}_rotated_{i}_{char}.bmp")
+                generate_offcenter(f, char).save(f"{OUTPUT}/{f_name}_offcenter_{i}_{char}.bmp")
+                generate_rotated(f, char).save(f"{OUTPUT}/{f_name}_rotated_{i}_{char}.bmp")
 
 
 if __name__ == "__main__":
