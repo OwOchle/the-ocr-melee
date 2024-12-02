29b882115e0754a6be02b9bc209e3c3c5ac3df43
Christina-Nora Michelard <christina.michelard@epita.fr>
2024-12-03 00:13:02 +0100

    feat(ui): moreButtons

--- ui/main.c
+++ ui/main.c
@@ -1,8 +1,7 @@
 #include <gtk/gtk.h>
 #include <stdio.h>
 
-GtkWidget *window = NULL;
-GtkLabel *mylabel = NULL;
+GtkWidget *mainWindow = NULL;
 
 int main(int argc, char *argv[])
 {
@@ -12,27 +11,53 @@ int main(int argc, char *argv[])
     builder = gtk_builder_new();
     gtk_builder_add_from_file(builder, "main.glade", NULL);
 
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "window0"));
-    mylabel = GTK_LABEL(gtk_builder_get_object(builder, "label0"));
+    mainWindow = GTK_WIDGET(gtk_builder_get_object(builder, "mainWindow"));
 
     gtk_builder_connect_signals(builder, NULL);
     g_object_unref(builder);
 
-    gtk_widget_show_all(window);
+    gtk_widget_show_all(mainWindow);
     gtk_main();
 
     return 0;
 }
 
-void exit_app()
+void exit_app() // Quand on ferme la fenêtre
 {
     printf("Exited app\n");
-    gtk_widget_destroy(window);
+    gtk_widget_destroy(mainWindow);
     gtk_main_quit();
 }
 
-void button0_clicked()
+void button0_clicked() // Tests
 {
     printf("Button clicked\n");
-    gtk_label_set_text(mylabel, "< insert cookie_image :3 >");
+    // gtk_label_set_text(mylabel, "< insert cookie_image :3 >");
+}
+
+void lrotButton_clicked() // Bouton de rotation gauche
+{
+    printf("Rotated left by 3 degrees\n");
+}
+
+void rrotButton_clicked() // Bouton de rotation droit
+{
+    printf("Rotated right by 3 degrees\n");
+}
+
+void solveButton_clicked() // Bouton qui solve
+{
+    printf("Solving...\n");
+}
+
+void rotButton_clicked() // Bouton qui rotate AUTO
+{
+    printf("Rotating...\n");
+}
+
+void stepButton_clicked() // Bouton qui solve en montrant chaque étape
+{
+    printf("------------------------------------------\n");
+    printf("Solving:\nStep 1 : ...\nStep 2 : ...\nStep 3 : ...\n");
+    printf("------------------------------------------\n");
 }
