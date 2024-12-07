20fce0b1897aede8d772189298bae40b2e7da303
Christina-Nora Michelard <christina.michelard@epita.fr>
2024-12-07 14:15:12 +0100

    feat(UI): squelette final done, fixes tbd

--- ui/main.c
+++ ui/main.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 
 GtkWidget *mainWindow = NULL;
-// GtkWidget *mylabel = NULL;
 GtkImage *mainImage = NULL;
+GtkWidget *solverWindow = NULL;
+GtkWidget *settingsWindow = NULL;
+GtkWidget *rotationWindow = NULL;
 
 int main(int argc, char *argv[])
 {
@@ -14,14 +16,18 @@ int main(int argc, char *argv[])
     gtk_builder_add_from_file(builder, "main.glade", NULL);
 
     mainWindow = GTK_WIDGET(gtk_builder_get_object(builder, "mainWindow"));
-    // mylabel = GTK_WIDGET(gtk_builder_get_object(builder, "label0"));
     mainImage = GTK_IMAGE(gtk_builder_get_object(builder, "mainImage"));
 
+    solverWindow = GTK_WIDGET(gtk_builder_get_object(builder, "solverWindow"));
+
+    settingsWindow = GTK_WIDGET(gtk_builder_get_object(builder, "settingsWindow"));
+
+    rotationWindow = GTK_WIDGET(gtk_builder_get_object(builder, "rotationWindow"));
 
     gtk_builder_connect_signals(builder, NULL);
     g_object_unref(builder);
 
-    gtk_widget_show_all(mainWindow);
+    gtk_widget_show(mainWindow);
     gtk_main();
 
     return 0;
@@ -31,26 +37,58 @@ void exit_app() // Quand on ferme la fenêtre
 {
     printf("Exited app\n");
     gtk_widget_destroy(mainWindow);
+    gtk_widget_destroy(solverWindow);
+    gtk_widget_destroy(settingsWindow);
+    gtk_widget_destroy(rotationWindow);
     gtk_main_quit();
 }
 
-void button0_clicked() // Tests
+void open_main() // Bouton qui ouvre le main menu
 {
-    printf("Button clicked\n");
-    // gtk_label_set_text(mylabel, "< insert cookie_image :3 >");
+    printf("Main menu opened\n");
+    gtk_widget_hide(solverWindow);
+    gtk_widget_hide(rotationWindow);
+    gtk_widget_hide(settingsWindow);
+    gtk_widget_show(mainWindow);
 }
 
-void lrotButton_clicked() // Bouton de rotation gauche
+void open_solver() // Bouton qui ouvre le solver
+{
+    printf("Solver opened\n");
+    gtk_widget_hide(mainWindow);
+    gtk_widget_show(solverWindow);
+}
+
+void open_settings() // Bouton qui ouvre les settings
+{
+    printf("Settings opened\n");
+    gtk_widget_hide(mainWindow);
+    gtk_widget_show(settingsWindow);
+}
+
+void open_rotation() // Bouton qui ouvre la fenêtre de rotation
+{
+    printf("Rotation window opened\n");
+    gtk_widget_hide(mainWindow);
+    gtk_widget_show(rotationWindow);
+}
+
+void leftRotation() // Bouton de rotation gauche
 {
     printf("Rotated left by 3 degrees\n");
 }
 
-void rrotButton_clicked() // Bouton de rotation droit
+void rightRotation() // Bouton de rotation droit
 {
     printf("Rotated right by 3 degrees\n");
 }
 
-void solveButton_clicked() // Bouton qui solve
+void automaticRotation() // Bouton de rotation droit
+{
+    printf("Rotating...\n");
+}
+
+void allInOne_clicked() // Bouton qui solve d'un coup
 {
     printf("Solving...\n");
 }
@@ -60,16 +98,21 @@ void rotButton_clicked() // Bouton qui rotate AUTO
     printf("Rotating...\n");
 }
 
-void stepButton_clicked() // Bouton qui solve en montrant chaque étape
+void stepByStep_clicked() // Bouton qui solve en montrant chaque étape
 {
     printf("------------------------------------------\n");
     printf("Solving:\nStep 1 : ...\nStep 2 : ...\nStep 3 : ...\n");
     printf("------------------------------------------\n");
 }
 
-void on_fileImport_set(GtkFileChooserButton *file)
+void on_imageImport(GtkFileChooserButton *file)
 {
     char *str = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(file));
     printf("imported file name = %s\n", str);
     gtk_image_set_from_file(mainImage,str);
+}
+
+void on_networkImput(GtkFileChooserButton *file)
+{
+
 }
