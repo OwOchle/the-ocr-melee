31ed48201a9dd67b90373cd730478f93fe671001
cryoxe <arthmal@icloud.com>
2024-12-08 16:47:38 +0100

    fix(read_image): corrected image loading

--- alphabet-training/read_image.c
+++ alphabet-training/read_image.c
@@ -7,10 +7,16 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 #include "read_image.h"
-// #include "../utils/array.h"
+#include "../utils/array.h"
 #include "../utils/verbose.h"
+
+#define get_pixel(surface, x, y) ((Uint8 *)surface->pixels + (y) * surface->pitch + (x) * surface->format->BytesPerPixel)
+
+#define IMAGE_SIZE 20
 
 regex_t filename_regex;
 
@@ -52,6 +58,8 @@ int __load_image(FILE *file, uchar *out)
     return 0;
 }
 
+
+
 int load_image(char *path, InputImage *output)
 {
     FILE *file = fopen(path, "r");
@@ -76,14 +84,14 @@ int load_image(char *path, InputImage *output)
 
     type[groups[2].rm_eo - groups[2].rm_so] = 0;
 
-    verbose_printf("type of %s is %s\n", output->name, type);
+    // verbose_printf("type of %s is %s\n", output->name, type);
 
     char character[255];
     strcpy(character, output->name + groups[4].rm_so);
 
     character[groups[4].rm_eo - groups[4].rm_so] = 0;
 
-    verbose_printf("char of %s is %s\n", output->name, character);
+    // verbose_printf("char of %s is %s\n", output->name, character);
 
     output->character = character[0];
 
@@ -102,7 +110,64 @@ int load_image(char *path, InputImage *output)
 
     fclose(file);
 
+    // print_matrix_tmp(output->image, IMAGE_SIZE);
+
     return res;
+}
+
+int load_image_SDL(const char *path, InputImage *output)
+{
+    SDL_Surface *image = IMG_Load(path);
+
+    if (image == NULL)
+    {
+        errx(1, "Could not load image at path '%s'", path);
+    }
+
+    size_t group_count = 5;
+    regmatch_t groups[group_count];
+    if (regexec(&filename_regex, output->name, group_count, groups, 0))
+    {
+        verbose_printf("invalid filename '%s'", output->name);
+    }
+
+    char type[255];
+
+    strcpy(type, output->name + groups[2].rm_so);
+
+    type[groups[2].rm_eo - groups[2].rm_so] = 0;
+
+    // verbose_printf("type of %s is %s\n", output->name, type);
+
+    char character[255];
+    strcpy(character, output->name + groups[4].rm_so);
+
+    character[groups[4].rm_eo - groups[4].rm_so] = 0;
+
+    // verbose_printf("char of %s is %s\n", output->name, character);
+
+    output->character = character[0];
+    output->category = IMG_PLAIN;
+
+    float *out = malloc(sizeof(float) * IMAGE_SIZE * IMAGE_SIZE);
+    for (size_t y = 0; y < image->h; y++)
+    {
+        for (size_t x = 0; x < image->w; x++)
+        {
+            Uint8 *pix = get_pixel(image, x, y);
+            array_get_as_matrix(out, IMAGE_SIZE, x, y) = (pix[0] + pix[1] + pix[2]) ? 1.0 : 0.0;
+        }
+    }
+
+    for (size_t i = 0; i < IMAGE_SIZE * IMAGE_SIZE; i++)
+    {
+        output->image[i] = out[i];
+    }
+    
+    // print_matrix_tmp(output->image, IMAGE_SIZE);
+    SDL_FreeSurface(image);
+
+    return 0;
 }
 
 InputImage *load_directory(char *path, size_t *count)
@@ -161,12 +226,12 @@ InputImage *load_directory(char *path, size_t *count)
 
         strcpy(output[i].name, entry->d_name);
 
-        if (load_image(f_path, output + i))
+        if (load_image_SDL(f_path, output + i))
         {
             verbose_printf("failed to load image %s\n", f_path);
             file_count--;
         }
-
+        // print_matrix_tmp((output + i)->image, IMAGE_SIZE);
         i++;
     }
 
